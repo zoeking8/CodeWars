809a8067e4a05a257de944e00b544f51c68a6f3c
Zoe King <zoe.king@razor.co.uk>
2019-08-16 13:23:33 +0100

    Add Complementary Dna

---
+++ src/CodeWars/ComplementaryDna.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+	public class ComplementaryDna
+	{
+		public static Dictionary<char, string> _lookup;
+		static ComplementaryDna()
+		{
+			_lookup = new Dictionary<char, string>
+			{
+
+				{'A', "T"},
+				{'T', "A"},
+				{'C', "G"},
+				{'G', "C"}
+			};
+		}
+		public static string MakeComplement(string dna)
+		{
+			return string.Concat(dna.Select(x => _lookup[x]));
+		}
+		//public static string MakeComplement(string dna)
+		//{
+		//    var temp = new StringBuilder();
+		//    for (var i = 0; i < dna.Length; i++)
+		//    {
+		//        temp.Append(_lookup[dna[i]]);
+		//    }
+		//    return temp.ToString();
+		//}
+	}
+}
---
+++ test/CodeWars.UnitTests/ComplementaryDnaTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.UnitTests
+{
+	public class ComplementaryDnaTests
+	{
+		[TestCase("ATTGC", "TAACG")]
+		[TestCase("GTAT", "CATA")]
+		[TestCase("TTTT", "AAAA")]
+		[TestCase("TAACG", "ATTGC")]
+		[TestCase("CATA", "GTAT")]
+		public void MakeComplement(string dna, string expected)
+		{
+			var actual = ComplementaryDna.MakeComplement(dna);
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+
+}
