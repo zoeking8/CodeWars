c8b207ef560762333ff1534818d123a887ff085f
Zoe King <zoe.king@razor.co.uk>
2019-08-19 08:45:26 +0100

    add count sheep

---
+++ src/CodeWars/CountSheepCantSleep.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+	public class CountSheepCantSleep
+	{
+		public static string CountSheep(int n)
+		{
+			var sheepCount = new StringBuilder();
+
+			for (int i = 1; i <= n; i++)
+			{
+				sheepCount.Append(i.ToString() + " sheep...");
+
+			}
+
+			return sheepCount.ToString();
+
+		} 
+	}
+}
---
+++ test/CodeWars.UnitTests/CountSheepCantSleepTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace CodeWars.UnitTests
+{
+	public class CountSheepCantSleepTests
+	{
+		[TestCase(1, "1 sheep...")]
+		[TestCase(2, "1 sheep...2 sheep...")]
+		[TestCase(3, "1 sheep...2 sheep...3 sheep...")]
+		public void CountSheep(int n, string expected)
+		{
+			var actual = CountSheepCantSleep.CountSheep(n);
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
