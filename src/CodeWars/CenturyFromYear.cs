d9cac2301955b203820dffcfeda51ad255800623
Zoe King <zoe.king@razor.co.uk>
2019-08-20 11:14:24 +0100

    Added century from year

---
+++ src/CodeWars/CenturyFromYear.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+	public class CenturyFromYear
+	{
+		public static int YearToCentury(int year)
+		{
+			return (int)Math.Ceiling(year / 100d);
+		}
+	}
+
+}
---
+++ test/CodeWars.UnitTests/CenturyFromYearTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.UnitTests
+{
+	public class CenturyFromYearTests
+	{
+		[TestCase(1705, 18)]
+		[TestCase(1900, 19)]
+		[TestCase(1601, 17)]
+		[TestCase(2000, 20)]
+
+
+		public void YearToCentury(int year, int expected)
+		{
+			var actual = CenturyFromYear.YearToCentury(year);
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
