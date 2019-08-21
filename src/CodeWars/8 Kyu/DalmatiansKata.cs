c522c3db3ec14eac8abfe91a84fd8ea6a99d9aab
Zoe King <zoe.king@razor.co.uk>
2019-08-21 15:16:04 +0100

    tail kata added

--- src/CodeWars/8 Kyu/DalmatiansKata.cs
+++ src/CodeWars/8 Kyu/DalmatiansKata.cs
@@ -1,21 +1,20 @@
-﻿using System;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-public static class DalmatiansKata
+﻿namespace CodeWars
 {
-	public static string HowManyDalmatians(int n)
+	public static class DalmatiansKata
 	{
-		if (n <= 10)
-			return "Hardly any";
+		public static string HowManyDalmatians(int n)
+		{
+			if (n <= 10)
+				return "Hardly any";
 
-		if (n <= 50)
-			return "More than a handful!";
+			if (n <= 50)
+				return "More than a handful!";
 
-		if (n == 101)
-			return "101 DALMATIONS!!!";
+			if (n == 101)
+				return "101 DALMATIONS!!!";
 
-		return "Woah that's a lot of dogs!";
+			return "Woah that's a lot of dogs!";
 
+		}
 	}
 }
---
+++ test/CodeWars.UnitTests/8 Kyu/TailTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.UnitTests
+{
+	public class TailTests
+	{
+		[TestCase("Fox", "x", true)]
+		[TestCase("Rhino", "o", true)]
+		[TestCase("Meerkat", "t", true)]
+		[TestCase("Emu", "t", false)]
+
+		public void CorrectTail(string body, string tail, bool expected)
+		{
+			var actual = TailKata.CorrectTail(body, tail);
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
